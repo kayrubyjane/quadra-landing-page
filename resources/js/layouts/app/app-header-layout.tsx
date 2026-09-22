44a7745b25edb22c783dc6262aca70f950f53aaa
kayrubyjane <arief.febrian26@gmail.com>
2026-09-22 16:42:13 +0700

    fix: change linter to biome

--- resources/js/layouts/app/app-header-layout.tsx
+++ resources/js/layouts/app/app-header-layout.tsx
@@ -1,16 +1,16 @@
-import { AppContent } from '@/components/app-content';
-import { AppHeader } from '@/components/app-header';
-import { AppShell } from '@/components/app-shell';
-import type { AppLayoutProps } from '@/types';
+import { AppContent } from "@/components/app-content";
+import { AppHeader } from "@/components/app-header";
+import { AppShell } from "@/components/app-shell";
+import type { AppLayoutProps } from "@/types";
 
 export default function AppHeaderLayout({
-    children,
-    breadcrumbs,
+	children,
+	breadcrumbs,
 }: AppLayoutProps) {
-    return (
-        <AppShell variant="header">
-            <AppHeader breadcrumbs={breadcrumbs} />
-            <AppContent variant="header">{children}</AppContent>
-        </AppShell>
-    );
+	return (
+		<AppShell variant="header">
+			<AppHeader breadcrumbs={breadcrumbs} />
+			<AppContent variant="header">{children}</AppContent>
+		</AppShell>
+	);
 }
--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,18 +1,18 @@
-import type { Auth } from '@/types/auth';
+import type { Auth } from "@/types/auth";
 
-declare module 'react' {
-    interface InputHTMLAttributes<T> {
-        passwordrules?: string;
-    }
+declare module "react" {
+	interface InputHTMLAttributes<T> {
+		passwordrules?: string;
+	}
 }
 
-declare module '@inertiajs/core' {
-    export interface InertiaConfig {
-        sharedPageProps: {
-            name: string;
-            auth: Auth;
-            sidebarOpen: boolean;
-            [key: string]: unknown;
-        };
-    }
+declare module "@inertiajs/core" {
+	export interface InertiaConfig {
+		sharedPageProps: {
+			name: string;
+			auth: Auth;
+			sidebarOpen: boolean;
+			[key: string]: unknown;
+		};
+	}
 }
